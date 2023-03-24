be4ab136cab221a8cff9ec84f0b62ad82acb3c24
Gen4ralz <76762117+Gen4ralz@users.noreply.github.com>
2023-03-24 18:52:07 +0700

    Connect frontend to backend & enable CORS

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *application) routes() http.Handler {
+
+	// create a router mux
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET","POST","PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: true,
+		MaxAge: 300,
+	}))
+
+	mux.Get("/", app.Home)
+	mux.Get("/movies", app.AllMovies)
+
+
+	return mux
+}
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,6 @@ module backend
 
 go 1.19
 
-require github.com/go-chi/chi/v5 v5.0.8 // indirect
+require github.com/go-chi/chi/v5 v5.0.8
+
+require github.com/go-chi/cors v1.2.1 // indirect
